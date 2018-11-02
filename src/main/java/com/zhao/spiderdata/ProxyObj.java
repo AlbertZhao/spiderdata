de94f78652f0f0a25a20d112ead2c320d9945922
albert.zhao <albert.chiu.cn@gmail.com>
2018-11-03 04:48:04 +0800

    add more samples

---
+++ src/main/java/com/zhao/spiderdata/ProxyObj.java
@@ -0,0 +1,23 @@
+package com.zhao.spiderdata;
+
+public class ProxyObj {
+
+    String proxyIP;
+    String proxyPort;
+
+    public String getProxyIP() {
+        return proxyIP;
+    }
+
+    public void setProxyIP(String proxyIP) {
+        this.proxyIP = proxyIP;
+    }
+
+    public String getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(String proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+}
---
+++ src/main/java/com/zhao/spiderdata/pojo/PersonInfo.java
@@ -0,0 +1,67 @@
+package com.zhao.spiderdata.pojo;
+
+public class PersonInfo {
+    String name;
+    String mobileNumber;
+    String age;
+    String sex;
+    String job;
+    String address;
+    String details;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
